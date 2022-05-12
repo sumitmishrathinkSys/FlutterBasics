95f52fb3fa2097472c6d9c24a9029d3adb5308a9
Sumit Mishra <mishra.sumit@thinksys.com>
2022-05-12 18:50:08 +0530

    Second commit

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final days = 30;
+    final name = "codepur";
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog App"),
+      ),
+        body: Center(
+            child: Container(
+      child: Text("Welcome to $days days Tutorial by $name "),
+    )),
+    drawer: Drawer(),
+    );
+  }
+} 
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter2_app/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    
+    
+    var day = "Tuesday";
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to Tutorial"),
-          )
-        )
-      )
-    );
+        home: HomePage(),);
   }
 }
