d5a3416a926da156b87eb94df0080a9dc011f3bc
Sumit Mishra <mishra.sumit@thinksys.com>
2022-05-16 12:52:18 +0530

    thirteen Commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter2_app/models/catalog.dart';
 import 'package:flutter2_app/widgets/drawer.dart';
+import 'package:flutter2_app/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
+  final days = 30;
+  final name = "Flutter";
   @override
   Widget build(BuildContext context) {
-    final days = 30;
-    final name = "codepur";
+    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Colors.white,
@@ -18,10 +20,19 @@ class HomePage extends StatelessWidget {
           // style: TextStyle(color: Colors.black),
         ),
       ),
-      body: Center(
-          child: Container(
-        child: Text("Welcome to $days days Tutorial by $name "),
-      )),
+      // ListView builder gives us recycler view which only render the items that are visible on screen and if we scroll the screen then it rendders the other items.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
+        ),
+      ),
+
       drawer: MyDrawer(),
     );
   }
